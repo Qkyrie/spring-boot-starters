0e06db7797f7f1a97b895a69935e5253d4919e54
Quinten De Swaef <quintendeswaef@gmail.com>
2017-02-02 16:44:16 +0100

    fix typos

--- scripts/continuous-integration/prepare-deploy-to-mvn.sh
+++ scripts/continuous-integration/prepare-deploy-to-mvn.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_64f39d4087b6_key -iv $encrypted_64f39d4087b6_iv -in scripts/continuous-integration/codesigning.asc.enc -out scripts/continuous-intragration/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_64f39d4087b6_key -iv $encrypted_64f39d4087b6_iv -in scripts/continuous-integration/codesigning.asc.enc -out scripts/continuous-integration/codesigning.asc -d
     gpg --fast-import scripts/continuous-integration/signingkey.asc
 fi
