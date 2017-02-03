ef065bd59a85173b3b0e78c2701839069853403c
Quinten De Swaef <quintendeswaef@gmail.com>
2017-02-03 07:41:24 +0100

    rename of the cert

--- scripts/continuous-integration/prepare-deploy-to-mvn.sh
+++ scripts/continuous-integration/prepare-deploy-to-mvn.sh
@@ -7,5 +7,5 @@
 if [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     echo "decrypting certificate"
     openssl aes-256-cbc -K $encrypted_64f39d4087b6_key -iv $encrypted_64f39d4087b6_iv -in scripts/continuous-integration/codesigning.asc.enc -out scripts/continuous-integration/codesigning.asc -d
-    gpg --fast-import scripts/continuous-integration/signingkey.asc
+    gpg --fast-import scripts/continuous-integration/codesigning.asc
 fi
